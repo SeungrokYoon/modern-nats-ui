80f630da8376c3756f6f80d560158c14069c3612
Seungrok <sonicrok93@gmail.com>
2023-02-07 05:26:17 +0900

    env: install & config eslint + prettier

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import HomePage from './index'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import HomePage from "./index";
 
-it('shows Welcome message.', () => {
-  render(<HomePage />)
-  const element = screen.getByRole('heading', {
-    name: 'Welcome to Next.js!',
-  })
-  expect(element).toBeInTheDocument()
-})
+it("shows Welcome message.", () => {
+  render(<HomePage />);
+  const element = screen.getByRole("heading", {
+    name: "Welcome to Next.js!",
+  });
+  expect(element).toBeInTheDocument();
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 function HomePage() {
-  return <div role="heading">Welcome to Next.js!</div>
+  return (
+    <div role="heading" aria-level={1}>
+      Welcome to Next.js!
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
